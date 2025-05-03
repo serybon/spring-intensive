dad246ef3197d5abcf5fbbf492a3243aa801b2bd
Siarhei Bondar <58183264+serybon@users.noreply.github.com>
2025-05-03 21:13:52 +0300

    + crud operations

--- src/main/java/by/bnd/intensive_spring/common/util/ServerResponseHelper.java
+++ src/main/java/by/bnd/intensive_spring/common/util/ServerResponseHelper.java
@@ -1,8 +1,10 @@
 package by.bnd.intensive_spring.common.util;
 
 import by.bnd.intensive_spring.model.ServerResponse;
+import by.bnd.intensive_spring.model.entity.Contact;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -39,4 +41,33 @@ public class ServerResponseHelper {
         return notFound(result, Collections.singletonList("Данные не найдены"));
     }
 
+    public static <T> ResponseEntity<ServerResponse<T>> conflict(T result, List<String> errors) {
+        HttpStatus httpStatus = HttpStatus.CONFLICT;
+        ServerResponse<T> response = ServerResponse.<T>builder()
+                .result(result)
+                .statusCode(httpStatus)
+                .errorMessages(errors)
+                .isSuccess(false)
+                .build();
+        return ResponseEntity
+                .status(httpStatus)
+                .body(response);
+    }
+
+    public static <T> ResponseEntity<ServerResponse<T>> conflict(T result) {
+        return conflict(result, Collections.singletonList("Конфликт при выполнении операции"));
+    }
+
+
+
+    public static <T> ResponseEntity<ServerResponse<T>> created(T result) {
+        HttpStatus httpStatus = HttpStatus.CREATED;
+        ServerResponse<T> response = ServerResponse.<T>builder()
+                .result(result)
+                .statusCode(httpStatus)
+                .isSuccess(true)
+                .errorMessages(new ArrayList<>())
+                .build();
+        return ResponseEntity.status(httpStatus).body(response);
+    }
 }
--- src/main/java/by/bnd/intensive_spring/controller/ContactController.java
+++ src/main/java/by/bnd/intensive_spring/controller/ContactController.java
@@ -3,15 +3,16 @@ package by.bnd.intensive_spring.controller;
 import by.bnd.intensive_spring.common.util.ServerResponseHelper;
 import by.bnd.intensive_spring.model.ServerResponse;
 import by.bnd.intensive_spring.model.entity.Contact;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("api/contact")
 public class ContactController {
+
     private ArrayList<Contact> contacts = new ArrayList<>();
 
     public ContactController() {
@@ -46,4 +47,44 @@ public class ContactController {
         return ServerResponseHelper.ok(contact);
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ServerResponse<Void>> deleteContact(@PathVariable int id) {
+        boolean canRemove = contacts.removeIf(contact -> contact.getId() == id);
+        if (canRemove) {
+            return ServerResponseHelper.ok(null);
+        } else return ServerResponseHelper.notFound(null,
+                Collections.singletonList("Контакт с указанным id не найден"));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ServerResponse<Contact>> addContact(
+            @RequestBody Contact contact) {
+        int newId = contacts.stream()
+                            .mapToInt(Contact::getId)
+                            .max().orElse(0) + 1;
+        boolean emailExists = contacts.stream()
+                .anyMatch(c -> c.getEmail().equalsIgnoreCase(contact.getEmail()));
+        if (emailExists) {
+            return ServerResponseHelper.conflict(null,
+                    Collections.singletonList("Контакт с таким email уже существует"));
+        }
+        contact.setId(newId);
+        contacts.add(contact);
+        return ServerResponseHelper.created(contact);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ServerResponse<Contact>> updateContact(
+            @RequestBody Contact contact) {
+
+        return contacts.stream()
+                .filter(c -> c.getId() == contact.getId())
+                .findFirst()
+                .map(existingContact -> {
+                    int indexOfExistingContact = contacts.indexOf(existingContact);
+                    contacts.set(indexOfExistingContact, contact);
+                    return ServerResponseHelper.ok(contact);
+                }).orElse(ServerResponseHelper.notFound(null,
+                        Collections.singletonList("Контакт для обновления с указанным id не найден")));
+    }
 }
